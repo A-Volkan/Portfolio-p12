d2a40da51a5ae4c39f7130df084ba4c3a6b1c00d
Asci Volkan <volknasc735@gmail.com>
2023-11-12 17:27:08 +0100

    final

--- src/SideComponents/CrochetComponent.js
+++ src/SideComponents/CrochetComponent.js
@@ -39,7 +39,7 @@ right: 2rem;
 const CrochetComponent = (props) => {
 
     const ref = useRef(null);
-    const hiddenRef = useRef(null);
+    const hiddenRef = useRef(null); // ref pour lelement que l'on masque ou affiche en fonction du defilement
 
     useEffect(() => {
 
@@ -48,10 +48,11 @@ const CrochetComponent = (props) => {
             let scrollPosition = window.pageYOffset; // fake deprecate msg
             let windowSize = window.innerHeight;
             let bodyHeight = document.body.offsetHeight;
-
+            //Calcul de la différence entre la hauteur totale du corps et la position de défilement
             let diff = Math.max(bodyHeight - (scrollPosition + windowSize))
+            //Pourcentage de cette différence par rapport à la plage totale disponible
             let diffP = (diff * 100) / (bodyHeight - windowSize);
-
+            //Ajuste sa position verticale en fonction du pourcentage calculé.
             ref.current.style.transform = `translateY(${-diffP}%)`
 
             if (window.pageYOffset > 5) {
